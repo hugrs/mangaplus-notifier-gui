65d462f9a447a65196beee69c8a03ba7911e640d
Hugo Ros <hugo.ros@protonmail.com>
2021-08-12 21:17:02 +0300

    listview: reorder interface

--- ui/listview.mli
+++ ui/listview.mli
@@ -3,10 +3,10 @@ type t
 val create : packing:(GObj.widget -> unit) -> t
 val connect_entry_clicked : t -> (string -> unit) -> unit
 
+val set_selected : t -> Proto.title -> bool -> unit
 (* We have to pass the whole title list here. This is because we need to map
 the plain ids in the selection to titles in protobuf form *)
-val update_selection : t -> all_titles:Proto.title list Async.Deferred.t 
-  -> int list -> unit Async.Deferred.t
-
-val set_selected : t -> Proto.title -> bool -> unit
+val update_selection : t -> all_titles:Proto.title list Async.Deferred.t -> 
+  int list -> unit Async.Deferred.t
+  
 val refresh : t -> Proto.title list -> unit Async.Deferred.t
