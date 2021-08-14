58f20a0d96e5bb4eabe5a4fe84d17a25919891e9
Hugo Ros <hugo.ros@protonmail.com>
2021-08-14 21:34:58 +0300

    move gui entry point to /bin

--- bin/main_gui.ml
+++ bin/main_gui.ml
@@ -1,12 +1,13 @@
 open Core
 open Lib
 
-let () =
+let init () =
   ignore (GMain.init ());
   let _ = GtkThread.start () in
   let titles = Api.fetch_all ()
     |> Async.Deferred.map ~f:(fun ts -> List.filter ts ~f:Proto.Title.is_english) in
-  let window = Window.create ~titles () in
+  let window = Ui.Window.create ~titles () in
   window#show ();
   Core.never_returns (Async.Scheduler.go ())
-  
+
+let () = init ()
